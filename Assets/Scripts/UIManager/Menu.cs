2e8dba0b2a161a1ad1af34e1ed042879da7a808f
ata kaleli <atakaleli123@gmail.com>
2024-02-17 10:51:00 +0200

    OpeningUI added

--- Assets/Scripts/UIManager/Menu.cs
+++ Assets/Scripts/UIManager/Menu.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject menuScreen;
+    [SerializeField] private GameObject howToPlayScreen;
+    [SerializeField] private GameObject creditsScreen;
+    
+
+
+    void OnClickOpenHowToPlayScreen()
     {
-        
+        menuScreen.SetActive(false);
+        howToPlayScreen.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnClickOpenCreditsScreen()
     {
-        
+        menuScreen.SetActive(false);
+        creditsScreen.SetActive(true);
     }
+
+    void OnClickCloseHowToPlayScreen()
+    {
+        menuScreen.SetActive(true);
+        howToPlayScreen.SetActive(false);
+    }
+
+    void OnClickCloseCreditsScreen()
+    {
+        menuScreen.SetActive(true);
+        howToPlayScreen.SetActive(false);
+    }
+
+
+    void OnClickStartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    void OnClickExitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
 }
